982bd82f05fbad051e272eabcf19c593fd82763a
renestl <renestl@gmail.com>
2017-08-29 15:33:16 -0500

    added header menu section

--- src/components/Header/TopBar.jsx
+++ src/components/Header/TopBar.jsx
@@ -8,6 +8,29 @@ class TopBar extends Component {
 				<section id="logo" className="top-bar_logo">
 					<a href="/"><img src="http://via.placeholder.com/100x100" alt="Company Brand" />Millie's Masks</a>
 				</section>
+				{/* HEADER MENU */}
+				<section id="menu">
+					<ul className="level-1">
+						<li className="level-1_item">
+							<a href="/" className="level-1_item_link">home</a>
+						</li>
+						<li className="level-1_item">
+							<a href="/catalog" className="level-1_item_link">catalog</a>
+						</li>
+						<li className="level-1_item">
+							<a href="/blog" className="level-1_item_link">blog</a>
+						</li>
+						<li className="level-1_item">
+							<a href="/about-us" className="level-1_item_link">about us</a>
+						</li>
+						<li className="level-1_item">
+							<a href="/contact" className="level-1_item_link">contact us</a>
+						</li>
+						<li className="level-1_item">
+							<a href="/client-login" className="level-1_item_link">my account</a>
+						</li>
+					</ul>
+				</section>
 			</div>
 		)
 	}
