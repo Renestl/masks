62fdd09a5402d01891f3ae678c881461fdcfa70c
renestl <renestl@gmail.com>
2017-08-29 16:12:05 -0500

    created showcase component

---
+++ src/components/Header/Showcase.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Showcase extends Component {
+	render() {
+		return (
+			<div className="showcase">
+				<img src="http://via.placeholder.com/1350x350" alt="showcase images" />
+			</div>
+		)
+	}
+}
+
+export default Showcase;
