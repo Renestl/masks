3c2fcf26c930023e469ebac46c130514958ee4ac
Renestl <renestl@gmail.com>
2017-08-29 22:33:22 -0500

    added featured component

--- src/components/Main/FrontPage/FrontPage.jsx
+++ src/components/Main/FrontPage/FrontPage.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Showcase from './Showcase';
+import Featured from './Featured';
 
 class FrontPage extends Component {
 	render() {
 		return (
 			<div className="front-page">
 				<Showcase />
+				<Featured />
 			</div>
 		)
 	}
