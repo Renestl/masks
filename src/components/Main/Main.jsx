1acb2945d278e13a495ac5e38e5d0ed012700a2e
Renestl <renestl@gmail.com>
2017-08-29 22:10:24 -0500

    created files for main section of site pages

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import FrontPage from './FrontPage/FrontPage';
+
+class Main extends Component {
+	render() {
+		return (
+			<div className="main">
+				<FrontPage />
+			</div>
+		)
+	}
+}
+
+export default Main;
