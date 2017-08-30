a6ae6812d56fe7843da46d509075a68d1542bba8
Renestl <renestl@gmail.com>
2017-08-29 22:45:51 -0500

    added featured structure

---
+++ src/components/Main/FrontPage/Featured.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+function FeaturedItem(props) {
+	var classNames= `featured_item`
+
+	return(
+		<div className={classNames}>
+			<a href={props.link}>
+				<img src="http://via.placeholder.com/275x230" alt={props.imgAlt} className="showcase_image" />
+				<p>{props.itemName}</p>
+				<h4>${props.price}</h4>
+				<button>Add To Cart</button>
+			</a>
+		</div>
+	)
+}
+
+class Featured extends Component {
+	render() {
+		return (
+			<div className="featured">
+				<h2>Featured Products</h2>
+				<div className="featured_items">
+					<FeaturedItem
+						imgAlt='New Mask'
+						itemName="New Blue Mask"
+						price="50.00"
+					/>
+					<FeaturedItem
+						imgAlt='New Mask 2'
+						itemName="New Black Mask"
+						price="150.00"
+					/>
+					<FeaturedItem
+						imgAlt='New Plate'
+						itemName="New Blue Plate"
+						price="25.00"
+					/>
+					<FeaturedItem
+						imgAlt='New Cup'
+						itemName="New Blue Cup"
+						price="20.00"
+					/>
+				</div>
+				
+			</div>
+		)
+	}
+}
+
+export default Featured;
