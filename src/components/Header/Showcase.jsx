25a8557a3522be9f72563e19c3891bd0269e60e6
renestl <renestl@gmail.com>
2017-08-29 21:23:03 -0500

    added showcase items

--- src/components/Header/Showcase.jsx
+++ src/components/Header/Showcase.jsx
@@ -4,7 +4,30 @@ class Showcase extends Component {
 	render() {
 		return (
 			<div className="showcase">
-				<img src="http://via.placeholder.com/1350x350" alt="showcase images" />
+				<div className="showcase_item showcase_item__color1">
+					<a href="/masks">
+						<span className="btn">Masks</span>
+						<div className="showcase_item_image">
+							<img src="http://via.placeholder.com/369x230" alt="Masks" className="showcase_image" />
+						</div>
+					</a>
+				</div>
+				<div className="showcase_item showcase_item__color2">
+					<a href="/plates">
+						<span className="btn">Plates</span>
+						<div className="showcase_item_image">
+							<img src="http://via.placeholder.com/369x230" alt="Plates" className="showcase_image" />
+						</div>
+					</a>
+				</div>
+				<div className="showcase_item showcase_item__color3">
+					<a href="/cups">
+						<span className="btn">Cups</span>
+						<div className="showcase_item_image">
+							<img src="http://via.placeholder.com/369x230" alt="Cups" className="showcase_image" />
+						</div>
+					</a>
+				</div>
 			</div>
 		)
 	}
