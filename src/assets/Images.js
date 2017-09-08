b87ec7342412cb52583e75a207b084ec9449547f
Renestl <renestl@gmail.com>
2017-09-08 18:43:18 -0500

    created image dictionary

---
+++ src/assets/Images.js
@@ -0,0 +1,14 @@
+const images = {
+	blackMask: {
+		imgName: 'Black Mask',
+		uri: require('./img/igor-ovsyannykov-178739.jpg'),
+		altText: 'Black Mask'
+	},
+	boatMask: {
+		imgName: 'Boat Masks',
+		uri: require('./img/pura-comunicacao-78218.jpg'),
+		altText: 'Masks inside a boat'
+	} 
+};
+
+export default images;
