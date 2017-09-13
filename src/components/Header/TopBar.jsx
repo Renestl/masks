e3893bddce0b97f185a4249b13c68d18b73bba8e
Renestl <renestl@gmail.com>
2017-09-13 07:44:58 -0500

    added navigation

--- src/components/Header/TopBar.jsx
+++ src/components/Header/TopBar.jsx
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+
+import images from '../../assets/Images';
 
 class TopBar extends Component {
 	render() {
 		return (
-			<div className="top-bar">
-				{/* HEADER BRAND */}
-				<section id="logo" className="top-bar_logo">
-					<a href="/"><img src="http://via.placeholder.com/100x100" alt="Company Brand" />Millie's Masks</a>
-				</section>
+			<div className="topbar">
+				<Navbar inverse collapseOnSelect fixedTop>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<a href="/">
+								<img src={images.logoWhite.uri} alt={images.logoWhite.altText} />
+							</a>
+						</Navbar.Brand>
+						<Navbar.Toggle />
+					</Navbar.Header>
+					
+					<Navbar.Collapse>
+						<Nav pullRight>
+							<NavItem href="/" className="level-1_item_link">home</NavItem>
+							<NavItem href="/catalog" className="level-1_item_link">catalog</NavItem>
+							<NavItem href="/blog" className="level-1_item_link">blog</NavItem>
+							<NavItem href="/about-us" className="level-1_item_link">about us</NavItem>
+							<NavItem href="/contact" className="level-1_item_link">contact us</NavItem>
+							<NavItem href="/client-login" className="level-1_item_link">my account</NavItem>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
 
-				{/* HEADER MENU */}
-				<section id="menu">
-					<ul className="level-1">
-						<li className="level-1_item">
-							<a href="/" className="level-1_item_link">home</a>
-						</li>
-						<li className="level-1_item">
-							<a href="/catalog" className="level-1_item_link">catalog</a>
-						</li>
-						<li className="level-1_item">
-							<a href="/blog" className="level-1_item_link">blog</a>
-						</li>
-						<li className="level-1_item">
-							<a href="/about-us" className="level-1_item_link">about us</a>
-						</li>
-						<li className="level-1_item">
-							<a href="/contact" className="level-1_item_link">contact us</a>
-						</li>
-						<li className="level-1_item">
-							<a href="/client-login" className="level-1_item_link">my account</a>
-						</li>
-					</ul>
-				</section>
 
 				{/* HEADER CART */}
 				<div id="header_cart">
