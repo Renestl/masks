0964004b5489b637bdf122b45a7fb6490a88fb33
Renestl <renestl@gmail.com>
2017-09-16 20:39:06 -0500

    installed font awesome

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
