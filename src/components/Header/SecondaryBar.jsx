61fa45e7eadd10a293d538f57c61d0652484dd47
Renestl <renestl@gmail.com>
2017-09-19 12:25:56 -0500

    Updated page layout structure

--- src/components/Header/SecondaryBar.jsx
+++ src/components/Header/SecondaryBar.jsx
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
-import {Col} from 'react-bootstrap';
+import {Grid,Row, Col} from 'react-bootstrap';
 
 class SecondaryBar extends Component {
 	render() {
 		return (
-			<div className='secondarybar'>
-				<Col xs={12} md={4} mdOffset={8}>
-					{/* HEADER CART */}
-					<div id="header_cart">
-						<a href="/cart" aria-label="View shopping cart">
+			<Grid className='secondarybar' fluid>
+				<Row>
+					{/* Possible Announcements Bar where the current mdOffset is */}
+					<Col xs={2} xsOffset={7} sm={1} smOffset={7} md={1} mdOffset={9} id="header_cart">
+						{/* HEADER CART */}
+							<a href="/cart" aria-label="View shopping cart">
+								<span 
+									className="icon fa fa-shopping-cart fa-lg fa-fw" aria-hidden="true"
+								>
+									<span id="cart_items">0</span>
+								</span>
+							</a>
+					</Col>
+					
+					<Col xs={2} sm={1} md={1} id="header_search">
+						{/* HEADER SEARCH */}
 							<span 
-								className="icon fa fa-shopping-cart fa-2x fa-fw" aria-hidden="true"
-							>
-								<span id="cart_items">0</span>
-							</span>
-						</a>
-					</div>
-				</Col>
-				
-
-				{/* HEADER SEARCH */}
-				<div id="header_search">
-					<span 
-						className="icon fa fa-search fa-2x fa-fw" aria-hidden="true"
-						aria-label="Search">
-					</span>
-
-					<form 
-						action="/search" 
-						method="GET" 
-						className="search_form"
-					>
-						<input type="text" id="search_field" placeholder="Search store" />
-						<button id="search_submit" type="submit" >
-							<span 
-								className="icon fa fa-search fa-2x fa-fw" aria-hidden="true"
+								className="icon fa fa-search fa-lg fa-fw" aria-hidden="true"
 								aria-label="Search">
 							</span>
-						</button>
-					</form>
-				</div>
-			</div>
+
+							{/* <form 
+								action="/search" 
+								method="GET" 
+								className="search_form"
+							>
+								<input type="text" id="search_field" placeholder="Search store" />
+								<button id="search_submit" type="submit" >
+									<span 
+										className="icon fa fa-search fa-2x fa-fw" aria-hidden="true"
+										aria-label="Search">
+									</span>
+								</button>
+							</form> */}
+					</Col>
+				</Row>	
+			</Grid>
 		)
 	}
 }
