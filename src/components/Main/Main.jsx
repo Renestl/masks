a752bb1b20de822a916a89c19683a4346c6c17f6
renestl <renestl@gmail.com>
2017-09-20 18:54:47 -0500

    Added products component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './Main.css';
 
 import FrontPage from './FrontPage/FrontPage';
+import Products from './Products/Products';
 
 class Main extends Component {
 	render() {
 		return (
 			<div className="main">
 				<FrontPage />
+				<Products />
 			</div>
 		)
 	}
