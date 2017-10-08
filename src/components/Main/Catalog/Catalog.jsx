80662e4e3cc1156c6c2cb18add4582c656dc89f3
Renestl <renestl@gmail.com>
2017-10-08 03:20:22 -0500

    Created structure for main catalog page

--- src/components/Main/Catalog/Catalog.jsx
+++ src/components/Main/Catalog/Catalog.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import {Grid } from 'react-bootstrap';
+import { Grid, Col, Row } from 'react-bootstrap';
 
 import { products } from '../../../data/DummyData';
 import { productList } from "../../Shared/Products/Products";
+
 class Catalog extends Component {
 	render() {
 		return (
-			<Grid>
-				Catalog
+			<Grid className="catalog">
+				<h2>Products</h2>
+				<Row className="products">
+					<Col>
+						<Row className="product-sort">
+							<Col sm={12}>
+								<ul>
+									<li><a href=""></a>All</li>
+									<li><a href=""></a>Masks</li>
+									<li><a href=""></a>Plates</li>
+									<li><a href=""></a>Cups</li>
+									<li><a href=""></a>Other</li>
+								</ul>
+							</Col>
+						</Row>
+						<Row className="product-list">
+								{productList}
+						</Row>
+					</Col>
+				</Row>
 			</Grid>
 		)
 	}
