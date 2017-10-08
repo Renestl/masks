162e33c75a68a221e41efd105b2ad42718b1a0d0
Renestl <renestl@gmail.com>
2017-10-07 20:22:04 -0500

    Renamed products to catalog

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './Main.css';
 
 import FrontPage from './FrontPage/FrontPage';
-import Products from './Products/Products';
+import Catalog from './Catalog/Catalog';
 
 class Main extends Component {
 	render() {
 		return (
 			<div className="main">
 				<FrontPage />
-				<Products />
+				<Catalog />
 			</div>
 		)
 	}
