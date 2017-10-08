215b823414e973670ac654edae29a47c2a2efdf4
Renestl <renestl@gmail.com>
2017-10-08 03:19:40 -0500

    Imported data and product template

--- src/components/Main/Catalog/Catalog.jsx
+++ src/components/Main/Catalog/Catalog.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Grid } from 'react-bootstrap';
 
+import { products } from '../../../data/DummyData';
+import { productList } from "../../Shared/Products/Products";
 class Catalog extends Component {
 	render() {
 		return (
