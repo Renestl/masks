6c20073fa98ac1ab401cb5bfe06780388daaa46a
Renestl <renestl@gmail.com>
2017-10-11 12:50:36 -0500

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -8,5 +9,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+	<BrowserRouter>
+    <App />
+	</BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
