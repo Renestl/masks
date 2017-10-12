868b939677f55ebc9ad0c230b5d9a1cd6ebb1e6f
Renestl <renestl@gmail.com>
2017-10-11 20:26:58 -0500

    Moved login to seperate component and added new components

--- src/components/Main/Account/Account.jsx
+++ src/components/Main/Account/Account.jsx
@@ -1,45 +1,15 @@
 import React, { Component } from 'react';
 import {Grid,Row, Col} from 'react-bootstrap';
 
+import ShoppingCart from './ShoppingCart';
+import Login from './Login';
+
 class Account extends Component {
 	render() {
 		return (
 			<Grid className="account">
-				<Row>
-					<Col sm={12}>
-						<h2>Login or Create an Account</h2>
-					</Col>
-				</Row>
-				
-				<Row>
-					<Col sm={12} md={6}>
-							<p>New here?</p>
-							<p>Registration is free and easy!</p>
-							<ul>
-								<li><span></span>Faster Checkout</li>
-								<li><span></span>Save Multiple Shipping Addresses</li>
-								<li><span></span>View and Track Orders and more</li>
-							</ul>
-						<button id="create_submit" type="submit">Create An Account</button>
-					</Col>
-
-					<Col sm={12} md={6}>
-					Already registered?
-						<form 
-							action="" 
-							method="" 
-							className="account_form"
-						>
-							<input type="text" id="email_field" placeholder="Email Address" />
-							<input type="text" id="phone_field" placeholder="Password" />
-
-							<button id="login_submit" type="submit">Sign In</button>
-							<a href="/forgot-password">Forgot your password?</a>
-						</form>
-					</Col>
-				</Row>
-
-				
+				<Login />
+				<ShoppingCart />
 			</Grid>
 		)
 	}
